c1b77fe5ccaa1dfc62bd282429a69669ecfbe83d
ShekarSoma4933 <shekarsoma4933@gmail.com>
2023-02-09 00:02:59 -0500

    ec2 instance creation

--- main.tf
+++ main.tf
@@ -1,31 +1,173 @@
 provider "aws" {
-     #region = "us-east-1"
+     region = "us-east-1"
 }
 
-variable "subnet_cidr_block" {
-    description = "subnet cidr block"
-}
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
+variable "my_ip" {}
+variable "instance_type" {}
+variable "public_key_location" {}
 
-resource "aws_vpc" "dev-vpc" {
-    cidr_block = "10.0.0.0/16"
+#1
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
     tags = {
-        Name: "dev-vpc"
+        Name: "${var.env_prefix}-vpc"
     }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-    vpc_id = aws_vpc.dev-vpc.id
+resource "aws_subnet" "myapp-subnet-1" { 
+    vpc_id = aws_vpc.myapp-vpc.id
     cidr_block = var.subnet_cidr_block
-    availability_zone = "us-east-1a"
+    availability_zone = var.avail_zone
     tags = {    
-        Name: "dev-subnet-1"
+        Name: "${var.env_prefix}-subnet-1"
     }
 }
+#2
+/*resource "aws_route_table" "myapp-route-table" {
+    vpc_id = aws_vpc.myapp-vpc.id
 
-output "dev-vpc-id" {
-    value = aws_vpc.dev-vpc.id
-}
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-gateway.id
+    }
+    tags = {
+        Name: "${var.env_prefix}-rtb"
+    }
+}*/
 
-output "dev-subnet-id" {
-    value = aws_subnet.dev-subnet-1.id
+resource "aws_internet_gateway" "myapp-gateway" {
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    tags = {
+        Name: "${var.env_prefix}-igw"
+    }
 }
+#3
+ /*resource "aws_route_table_association" "aws-rtb-subnet-association" {
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    route_table = aws_route_table.myapp-route-table.id
+ }*/
+
+ #4
+ resource "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+    
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-gateway.id
+    }
+    tags = {
+        Name: "${var.env_prefix}-main-rtb"
+    }
+ }
+
+ #5
+ /*resource "aws_security_group" "myapp-sg" {
+    name = "myapp-sg"
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0 
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+
+    tags = {
+        Name: "${var.env_prefix}-sg"
+    }
+ }*/
+#6
+ resource "aws_default_security_group" "myapp-default-sg" {
+
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0 
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+    tags = {
+        Name: "${var.env_prefix}-default-sg"
+    }
+ }
+#7
+ data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+ }
+
+ resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+    
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_default_security_group.myapp-default-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name
+
+    /*ebs_block_device {
+    device_name = "/dev/xvda"
+    volume_size = 8
+    }*/
+    user_data = file("sample.sh")
+
+    tags = {
+        Name: "${var.env_prefix}-server"
+    }
+ }
+
+ resource "aws_key_pair" "ssh-key" {
+    key_name= "server-key"
+    public_key = file(var.public_key_location)
+ }
+
+ output "aws_ami_id"{
+    value = data.aws_ami.latest-amazon-linux-image.id
+ }
+  output "aws_public_ip"{
+    value = aws_instance.myapp-server.public_ip
+ }
+
+ #8
